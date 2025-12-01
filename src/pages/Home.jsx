35afa23777fc42c13a65bab0709dd6ef645d6b09
AJIMANO2002 <ajimano2002@gmail.com>
2025-12-01 19:11:42 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { TypeAnimation } from 'react-type-animation';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Ajith from '../assets/ajith03.jpeg';
+import Ajith from '../assets/ajith03.png';
 
 function Home() {
   return (
